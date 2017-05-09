83629b0129766ff9781e527472dcf49ec58ae173
ecantor12 <esteban.cantor@gmail.com>
2017-05-09 17:41:09 -0500

    load home and avg

--- reports/views.py
+++ reports/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def reports(request):
-	return render(request, 'reports/home.html', {})
+	from reports.tools import avgTime
+	avg_time = avgTime()
+	avg_time = round(avg_time[0]['promedio_citas_medicas'], 2)
+
+	return render(request, 'reports/home.html', {"avg_time": avg_time})
+
+
+#def avg():
+	
