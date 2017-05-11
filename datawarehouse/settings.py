49c9426e79974c41a4a3a10b3d2df5caff5dc115
Hernán <hernan@localhost.localdomain>
2017-05-11 07:48:53 -0500

    guardando datos

--- datawarehouse/settings.py
+++ datawarehouse/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'datawarehouse',
-        'USER': 'nmedios',
-        'PASSWORD': 'nu3v0sm3d10s',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
