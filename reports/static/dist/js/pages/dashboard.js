f397c27c468086b486a4c0d459c3f10a03513339
ecantor12 <esteban.cantor@gmail.com>
2017-05-11 04:55:56 -0500

    update

--- reports/static/dist/js/pages/dashboard.js
+++ reports/static/dist/js/pages/dashboard.js
@@ -49,22 +49,19 @@ $(function () {
   $(".knob").knob();
 
   //jvectormap data
-  var visitorsData = {
-    "US": 398, //USA
-    "SA": 400, //Saudi Arabia
-    "CA": 1000, //Canada
-    "DE": 500, //Germany
-    "FR": 760, //France
-    "CN": 300, //China
-    "AU": 700, //Australia
-    "BR": 600, //Brazil
-    "IN": 800, //India
-    "GB": 320, //Great Britain
-    "RU": 3000 //Russia
+ /*var visitorsData = {
+    "CO-AMA": 1156,
+    "CO-ANT": 1157,
+    "CO-ARA": 1148,
+    "CO-ATL": 1144,
+    "CO-BOL": 1136,
+    "CO-BOY": 172,
+    "CO-CAL": 1158
   };
+  //console.log(visitorsData);
   //World map by jvectormap
   $('#world-map').vectorMap({
-    map: 'world_mill_en',
+    map: 'co_mill',
     backgroundColor: "transparent",
     regionStyle: {
       initial: {
@@ -86,7 +83,7 @@ $(function () {
       if (typeof visitorsData[code] != "undefined")
         el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
     }
-  });
+  });*/
 
   //Sparkline charts
   var myvalues = [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021];
@@ -124,27 +121,28 @@ $(function () {
 
   /* Morris.js Charts */
   // Sales chart
-  var area = new Morris.Area({
-    element: 'revenue-chart',
-    resize: true,
-    data: [
-      {y: '2011 Q1', item1: 2666, item2: 2666},
-      {y: '2011 Q2', item1: 2778, item2: 2294},
-      {y: '2011 Q3', item1: 4912, item2: 1969},
-      {y: '2011 Q4', item1: 3767, item2: 3597},
-      {y: '2012 Q1', item1: 6810, item2: 1914},
-      {y: '2012 Q2', item1: 5670, item2: 4293},
-      {y: '2012 Q3', item1: 4820, item2: 3795},
-      {y: '2012 Q4', item1: 15073, item2: 5967},
-      {y: '2013 Q1', item1: 10687, item2: 4460},
-      {y: '2013 Q2', item1: 8432, item2: 5713}
-    ],
-    xkey: 'y',
-    ykeys: ['item1', 'item2'],
-    labels: ['Item 1', 'Item 2'],
-    lineColors: ['#a0d0e0', '#3c8dbc'],
-    hideHover: 'auto'
-  });
+  //var area = new Morris.Area({
+  //  element: 'revenue-chart',
+  //  resize: true,
+  //  data: [
+  //    {y: '2011 Q1', item1: 2666, item2: 2666},
+  //    {y: '2011 Q2', item1: 2778, item2: 2294},
+  //    {y: '2011 Q3', item1: 4912, item2: 1969},
+  //    {y: '2011 Q4', item1: 3767, item2: 3597},
+  //    {y: '2012 Q1', item1: 6810, item2: 1914},
+  //    {y: '2012 Q2', item1: 5670, item2: 4293},
+  //    {y: '2012 Q3', item1: 4820, item2: 3795},
+  //    {y: '2012 Q4', item1: 15073, item2: 5967},
+  //    {y: '2013 Q1', item1: 10687, item2: 4460},
+  //    {y: '2013 Q2', item1: 8432, item2: 5713}
+  //  ],
+  //  xkey: 'y',
+  //  ykeys: ['item1', 'item2'],
+  //  labels: ['Item 1', 'Item 2'],
+  //  lineColors: ['#a0d0e0', '#3c8dbc'],
+  //  hideHover: 'auto'
+  //});
+  /*
   var line = new Morris.Line({
     element: 'line-chart',
     resize: true,
@@ -174,19 +172,19 @@ $(function () {
     gridTextFamily: "Open Sans",
     gridTextSize: 10
   });
-
+  */
   //Donut Chart
-  var donut = new Morris.Donut({
-    element: 'sales-chart',
-    resize: true,
-    colors: ["#3c8dbc", "#f56954", "#00a65a"],
-    data: [
-      {label: "Download Sales", value: 12},
-      {label: "In-Store Sales", value: 30},
-      {label: "Mail-Order Sales", value: 20}
-    ],
-    hideHover: 'auto'
-  });
+  //var donut = new Morris.Donut({
+  //  element: 'sales-chart',
+  //  resize: true,
+  //  colors: ["#3c8dbc", "#f56954", "#00a65a"],
+  //  data: [
+  //    {label: "Download Sales", value: 12},
+  //    {label: "In-Store Sales", value: 30},
+  //    {label: "Mail-Order Sales", value: 20}
+  //  ],
+  //  hideHover: 'auto'
+  //});
 
   //Fix for charts under tabs
   $('.box ul.nav a').on('shown.bs.tab', function () {
--- reports/views.py
+++ reports/views.py
@@ -1,14 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext, loader
 
 # Create your views here.
 
 def reports(request):
-	from reports.tools import avgTime
-	avg_time = avgTime()
-	avg_time = round(avg_time[0]['promedio_citas_medicas'], 2)
-
-	return render(request, 'reports/home.html', {"avg_time": avg_time})
-
-
-#def avg():
+	from reports.tools import avgTimeMedical, avgTimeSurgeries, patientCare, patientCarePeriodYear
+	_dict = {}
+	provinces = {
+		"Valle del Cauca": "CO-VAC",
+		"Bolivar": "CO-BOY",
+		"Cesar": "CO-COR",
+		"Huila": "CO-HUI",
+		"Vaupes": "CO-VAU",
+		"Norte de Santander": "CO-NSA",
+		"Risaralda": "CO-RIS",
+		"Vichada": "CO-VID",
+		"Bogota": "CO-BOL",
+		"Choco": "CO-CUN",
+		"Guaviare": "CO-GUV",
+		"Casanare":"CO-CAU",
+		"Caqueta":"CO-CAS",
+		"Caldas": "CO-CAQ",
+		"Cauca": "CO-CES",
+		"Santander": "CO-SAN",
+		"Atlantico":"CO-ATL",
+		"Amazonas": "CO-AMA",
+		"Meta": "CO-MET",
+		"Magdalena": "CO-MAG",
+		"Arauca":"CO-ARA",
+		"Guainia":"CO-GUA",
+		"San Andres y Providencia":"CO-SAP",
+		"Boyaca": "CO-CAL",
+		"Quindio": "CO-QUI",
+		"La Guajira": "CO-LAG",
+		"Tolima": "CO-TOL",
+		"Sucre": "CO-SUC",
+		"Putumayo": "CO-PUT",
+		"Narino": "CO-NAR",
+		"Cordoba": "CO-CHO",
+		"Cundinamarca": "CO-DC",
+		"Antioquia": "CO-ANT",
+	}
 	
+	avg_time_medical = avgTimeMedical()
+	avg_time_medical = round(avg_time_medical[0]['promedio_citas_medicas'], 2)
+
+	avg_time_surgeries = avgTimeSurgeries()
+	avg_time_surgeries = round(avg_time_surgeries[0]['tiempo_espera_urgencia'], 2)
+	
+	hospitals, regions = patientCare()
+	by_clinic, by_medico = patientCarePeriodYear()
+	
+	for i in regions:
+		if provinces[i['departamento']]:
+			_dict[provinces[i['departamento']]] = int(i['suma'])
+	
+	"""
+    data_graphic = {
+        "labels": [],
+        "datasets": [],
+    }
+    data_set = {
+        "label": "",
+        "type": "",
+        "data": "",
+        "backgroundColor": "#F7BE81",
+    }
+
+    list_data_set = []
+    index = 0
+    for c in by_clinic:
+        #index += 1
+        data_label = []
+        for i in list_data:
+            for j in i["data"]:
+                if j["label"] == c.name:
+                    data_label.append(j["percentage"])
+        data_set["label"] = c.name
+        data_set["type"] = "bar"
+        data_set["data"] = data_label
+        data_set["backgroundColor"] = list_colors[index]
+        list_data_set.append(dict.copy(data_set))
+        index += 1
+    data_graphic["labels"] = labels
+    data_graphic["datasets"] = list_data_set
+    """
+
+	variables = RequestContext(request, {
+		"avg_time_medical": avg_time_medical,
+		"avg_time_surgeries": avg_time_surgeries,
+		"hospitals": hospitals,
+		"regions": _dict,
+		"by_clinic": by_clinic,
+		"by_medico": by_medico,
+		})
+	return render_to_response("reports/home.html", variables)
